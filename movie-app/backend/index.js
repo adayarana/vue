b62cbbf7f1cb6cd5b667c9cea9bb0d0fc7e9205a
Aday <aday69@hotmail.com>
2021-09-13 10:30:49 +0100

    Create index.js

---
+++ movie-app/backend/index.js
@@ -0,0 +1,28 @@
+const chalk = require('chalk');
+const cors = require('cors');
+const debug = require('debug')('server');
+const express = require('express');
+const morgan = require('morgan');
+
+require('dotenv').config();
+
+require('./src/config/ddbb.config');
+
+const routes = require('./src/routes/movie.routes');
+const userRoutes = require('./src/routes/user.routes');
+
+const server = express();
+const port = process.env.PORT || 4000;
+
+server.use(cors());
+server.use(morgan('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+
+server.use('/movie-app', routes);
+server.use(
+  '/movie-app/user',
+  userRoutes
+);
+
+server.listen(port, () => debug(`Server is running in ${chalk.blue(`http://localhost:${port}`)}`));
