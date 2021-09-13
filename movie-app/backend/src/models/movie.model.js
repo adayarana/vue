27b71335dbb356ac133494395cc389ec25529c2b
Aday <aday69@hotmail.com>
2021-09-13 10:39:08 +0100

    Create Model

---
+++ movie-app/backend/src/models/movie.model.js
@@ -0,0 +1,25 @@
+const { model, Schema } = require('mongoose');
+
+const movieSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+}).method('toJSON', function toJson() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = model('Movies', movieSchema);
