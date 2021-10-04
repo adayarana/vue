468ac12e9943651392f1ca546b9c5a93f43e1e17
Aday <aday69@hotmail.com>
2021-10-04 13:10:23 +0100

    Create Home view

--- movie-app/frontend/src/views/Home.vue
+++ movie-app/frontend/src/views/Home.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="home">
+    <Shows />
+    <People />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { mapActions, mapState } from 'vuex';
+import Shows from '../components/Shows.vue';
+import People from '../components/People.vue';
 
 export default {
   name: 'Home',
   components: {
+    Shows,
+    People
+  },
+  computed: {
+    ...mapState([
+      'shows',
+      'people'
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'getApiShows',
+      'getApiPeople'
+    ])
+  },
+  mounted() {
+    this.getApiShows();
+    this.getApiPeople();
   }
 };
 </script>
