7ba6dc0743ccfca4c777be300b6e9ea04f6432fe
Aday <aday69@hotmail.com>
2021-10-04 13:10:41 +0100

    Create People component

---
+++ movie-app/frontend/src/components/People.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="people-container">
+        <p>Soy People</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'People'
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
