17397a27778965d2804a4a6579db395bfa759329
Aday <aday69@hotmail.com>
2021-10-04 13:10:34 +0100

    Create Shows component

---
+++ movie-app/frontend/src/components/Shows.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="shows-container">
+        <div
+        v-for="(show, index) in shows"
+        :key="index"
+        >
+            <h3>{{ show.name }}</h3>
+            <figure>
+                <img v-bind:src="show.image.medium" alt="">
+                <figcaption></figcaption>
+            </figure>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Shows',
+  computed: {
+    ...mapState([
+      'shows'
+    ])
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
