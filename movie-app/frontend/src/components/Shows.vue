d83008d47aa19fe13108e23dbb903624bf0cf1d5
Aday <aday69@hotmail.com>
2021-10-05 12:23:01 +0100

    Add shows slider

--- movie-app/frontend/src/components/Shows.vue
+++ movie-app/frontend/src/components/Shows.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="shows-container">
       <div v-if="shows.length > 0">
-        <div
-        v-for="(show, index) in shows"
-        :key="index"
-        >
-          <img :src="show.image.medium" :alt="show.name">
-        </div>
+        <section class="shows-container__slider">
+          <figure class="slider__figure">
+            <img
+            v-for="(show, index) in shows"
+            :key="index"
+            class="figure__img-slider"
+            :src="show.image.medium"
+            :alt="show.name">
+          </figure>
+        </section>
       </div>
       <div v-else>
-        <p>Loading...</p>
+        <p>Loading Shows...</p>
       </div>
     </div>
 </template>
@@ -29,5 +33,78 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.shows-container {
+  width: 100%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  overflow: hidden;
+
+  .shows-container__slider {
+    overflow: hidden;
+    cursor: pointer;
+  }
+
+  // Set up width
+
+  .slider__figure {
+    position: relative;
+    width: 30000%;
+    margin: 0;
+    left: 0;
+    animation: 20s slider infinite;
+  }
+
+  .figure__img-slider {
+    float: left;
+  }
+
+}
+
+@media screen and (min-width: 415px) {
+  .slider__figure {
+    width: 1500%
+  }
+}
+
+@keyframes slider {
+  0% {
+    left: 0;
+  }
+
+  20% {
+    left: 0;
+  }
+
+  25% {
+    left: -100%;
+  }
+
+  45% {
+    left: -100%;
+  }
+
+  50% {
+    left: -200%;
+  }
+
+  70% {
+    left: -200%;
+  }
+
+  75% {
+    left: -300%;
+  }
+
+  95% {
+    left: -300%;
+  }
+
+  100% {
+    left: -400%;
+  }
+}
 
 </style>
